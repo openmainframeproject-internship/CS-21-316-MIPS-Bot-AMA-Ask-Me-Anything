48c1c5e214b42d032abf2eeda79052fa4701f84a
ameliong2217 <36515675+ameliong2217@users.noreply.github.com>
2020-10-29 18:03:49 -0400

    Add files via upload

---
+++ src/Resources/routeFind.js
@@ -0,0 +1,22 @@
+const mongodb = require("mongodb");
+let client = null;
+
+
+async function getDefByTerm(mongoRL, term) {
+
+    client = await mongodb.MongoClient.connect(mongoRL);
+    const qRes = await client.db('botdb').collection('zos').findOne({ Term: term });
+
+    return { defined: qRes.Definition }   
+}
+
+function main(params) {
+    mongoRL='mongodb://cs21-316:watsonBotsonMango613@dbaas231.hyperp-dbaas.cloud.ibm.com:28110/botdb?authSource=admin&replicaSet=watbot&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+
+    switch(params.hook) {
+        case "findByTerm":
+            return getDefByTerm(mongoRL, params.term);
+        default:
+            return { dberror: "No action defined", hook: params.hook}
+    }
+}
